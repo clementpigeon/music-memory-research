5f3045b73d65cdf553e238230fb4f8b485636fea
Clement Pigeon <clementpigeon@gmail.com>
2013-10-06 21:18:51 -0400

    filters songs by first letters

--- freebase-input.js
+++ freebase-input.js
@@ -1,8 +1,10 @@
 FREEBASE_API_URL = 'https://www.googleapis.com/freebase/v1/search';
 API_KEY = 'AIzaSyDeTSD9Zppyd23wCWkvrr4GhnjzhCCTHIs';
+var current_band_mid;
 
 var populateSelect = function(songsLookupResponse){
   var songs = songsLookupResponse.result;
+  console.log(songs);
   var newOptions = '';
 
   songs.forEach(function(song){
@@ -13,40 +15,51 @@ var populateSelect = function(songsLookupResponse){
   });
 
   $('select#song').html(newOptions);
+  console.log('populating done');
 }
 
 
-var retrieveSongs = function(bandLookupResponse) {
-  var bands = bandLookupResponse.result;
-  if (bands.length < 1){
-    $('div#result').html("Can't find that band. Please enter another name.");
-    return
-  }
-  var name = bands[0].name;
-  var mid = bands[0].mid;
-
-  $('div#result').html('You selected: ' + name);
-
+var update_song_list = function() {
   var songsLookupParams = {
     'filter':
-      '(all type:/music/recording /music/recording/artist:"' + mid + '")',
-    'limit': 50,
+      '(all type:/music/recording /music/recording/artist:"' + current_band_mid + '")',
+    'limit': 10,
     'indent': true,
     'spell': 'always',
     'key': API_KEY
   };
+  if ($('input#song_letters').val().length > 1){
+    songsLookupParams['query'] = $('input#song_letters').val();
+    songsLookupParams['prefixed'] = "true";
+  };
+  console.log(songsLookupParams);
   $.ajax({
     dataType: "json",
     url: FREEBASE_API_URL,
     data: songsLookupParams,
     success: populateSelect,
-
   });
 
 }
 
-var checkBand = function(){
-  // var url = 'https://www.googleapis.com/freebase/v1/search';
+var bandLookUpSuccess = function(response){
+  var bands = response.result;
+  if (bands.length < 1){
+    $('div#result').html("Can't find that band. Please enter another name.");
+    $('.songSelectView').hide();
+    return
+  }
+  var name = bands[0].name;
+  current_band_mid = bands[0].mid;
+
+  $('div#result').html('<br>Selected band: '+ name +'<br>Please select the song (type first letters to update list)');
+  $('.songSelectView').show();
+  update_song_list();
+}
+
+
+
+var bandLoopkup = function(){
   var query = $('#band').val();
   var bandLookupParams = {
     'query': query,
@@ -60,18 +73,28 @@ var checkBand = function(){
     dataType: "json",
     url: FREEBASE_API_URL,
     data: bandLookupParams,
-    success: retrieveSongs
+    success: bandLookUpSuccess
   });
 
 };
+
+
+var new_letter_callback = function(e){
+  var current_letters = $('input#song_letters').val();
+  update_song_list();
+  console.log(current_letters);
+}
+
 
 var songValue = function(){
   var song_mid = $('select#song').val();
   $('#newSong').html('selected song mid: ' + song_mid);
 }
 
-$('button#search').on('click', checkBand)
-$('button#send').on('click', songValue)
+
+$('button#search').on('click', bandLoopkup);
+$('input#song_letters').on('keyup', new_letter_callback);
+$('button#send').on('click', songValue);
 
 // thumbnail link:
 // https://www.googleapis.com/freebase/v1/image/m/04lxt8?key=AIzaSyCQVC9yA72POMg2VjiQhSJQQP1nf3ToZTs&maxwidth=125&maxheight=125&mode=fillcropmid&errorid=%2Ffreebase%2Fno_image_png
